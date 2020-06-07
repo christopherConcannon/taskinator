01da950b2aef55b6aa70cda4034eb64f9f9e7473
xtophe xanon <xocon10@gmail.com>
2020-06-07 17:52:33 -0500

    load tasks from localStorage on page load and populate page

--- assets/js/script.js
+++ assets/js/script.js
@@ -223,13 +223,14 @@ var deleteTask = function(taskId) {
   // loop through current tasks
   for (var i = 0; i < tasks.length; i++) {
     // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
-    if (tasks[i].id !== parseInt(taskIdO)) {
+    if (tasks[i].id !== parseInt(taskId)) {
       updatedTaskArr.push(tasks[i]);
     }
   }
-  saveTasks();
   // reassign tasks array to be the same as updatedTasksArr
   tasks = updatedTaskArr;
+  
+  saveTasks();
 };
 
 var dragTaskHandler = function(event) {
@@ -287,6 +288,49 @@ var saveTasks = function() {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+var loadTasks = function() {
+  // Get task items from localStorage
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+    return false;
+  } 
+    
+  tasks = JSON.parse(localStorage.getItem('tasks'));
+  console.log(tasks);
+
+  for (i = 0; i < tasks.length; i++) {
+    tasks[i].id === taskIdCounter;
+
+    var listItemEl = document.createElement('li');
+    listItemEl.className = 'task-item';
+    listItemEl.setAttribute('data-task-id', tasks[i].id);
+    listItemEl.setAttribute('draggable', 'true');
+
+    var taskInfoEl = document.createElement('div');
+    taskInfoEl.className = 'task-info';
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    
+    var tasksActionEl = createTaskActions(tasks[i].id);
+    listItemEl.appendChild(tasksActionEl);
+
+    if (tasks[i].status === 'to do') {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+      tasksToDoEl.appendChild(listItemEl);
+    } else if (tasks[i].status === 'in progress') {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+      tasksInProgressEl.appendChild(listItemEl);
+    } else if (tasks[i].status === 'completed') {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+      tasksCompletedEl.appendChild(listItemEl);
+    }
+    taskIdCounter++;
+  }
+  
+  // Convert tasks from stringified format back into array of objects
+  // Iterate through task array and create task elements on page from array data
+}
+
 pageContentEl.addEventListener('click', taskButtonHandler);
 formEl.addEventListener('submit', taskFormHandler);
 pageContentEl.addEventListener('change', taskStatusChangeHandler);
@@ -295,9 +339,12 @@ pageContentEl.addEventListener('dragover', dropZoneDragHandler);
 pageContentEl.addEventListener('drop', dropTaskHandler);
 pageContentEl.addEventListener('dragleave', dragLeaveHandler);
 
+loadTasks();
+
 // QUESTIONS
 // why do we return false on line 15? instead of just return.
 
 // why aren't we resetting task type select to default on submit?
 
 // if you have more than one draggable element type, how do you associate the correct dataTransfer property to the type in question, since the key of the reference is a generic data type ('text/plain') rather than a unique string?
+
