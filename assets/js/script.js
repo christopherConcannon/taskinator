3e941b5569c99ebacb2a638403dfb28c6bac2d26
xtophe xanon <xocon10@gmail.com>
2020-06-07 14:56:43 -0500

    improved UX for drag/drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -206,6 +206,7 @@ var dropZoneDragHandler = function(event) {
   var taskListEl = event.target.closest('.task-list');
   if(taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute('style', 'background: rgba(68, 233, 255, 0.7); border-style: dashed;');
   }
 }
 
@@ -224,7 +225,16 @@ var dropTaskHandler = function(event) {
     statusSelectEl.selectedIndex = 2;
   }
 
+  dropZoneEl.removeAttribute('style');
+
   dropZoneEl.appendChild(draggableElement);
+}
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest('.task-list');
+  if (taskListEl) {
+    taskListEl.removeAttribute('style');
+  }
 }
 
 pageContentEl.addEventListener('click', taskButtonHandler);
@@ -233,6 +243,7 @@ pageContentEl.addEventListener('change', taskStatusChangeHandler);
 pageContentEl.addEventListener('dragstart', dragTaskHandler);
 pageContentEl.addEventListener('dragover', dropZoneDragHandler);
 pageContentEl.addEventListener('drop', dropTaskHandler);
+pageContentEl.addEventListener('dragleave', dragLeaveHandler);
 
 // QUESTIONS
 // why do we return false on line 15? instead of just return.
